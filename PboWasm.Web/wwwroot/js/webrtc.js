f3a4f2beb8aaaaf6d45be13677bab7051a25e07e
DIMOTRANS\rperrin <rperrin@dimotrans-group.com>
2026-08-05 16:18:09 +0200

    Add TURN servers for enterprise NAT traversal

--- PboWasm.Web/wwwroot/js/webrtc.js
+++ PboWasm.Web/wwwroot/js/webrtc.js
@@ -9,7 +9,24 @@ window.webrtcInterop = {
         this.dotnetHelper = dotnetHelper;
 
         const config = {
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+            iceServers: [
+                { urls: "stun:stun.l.google.com:19302" },
+                {
+                    urls: "turn:openrelay.metered.ca:80",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                }
+            ]
         };
 
         this.peerConnection = new RTCPeerConnection(config);
